291fc74392c517d37a0605c8f07a77b42bfee606
nmktad <nmktadesse@gmail.com>
2023-05-13 21:37:46 +0300

    add t3env package for validating and managing env

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import "./src/lib/env.mjs";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
+};
+
+export default nextConfig;
